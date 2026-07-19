64ca36ec2562a7835b79c9bc1efe09cff21a642e
taliaduvet <hey@taliaduvet.com>
2026-07-19 11:52:19 -0700

    Add push notification support

--- Hub App/hub/src/App.tsx
+++ Hub App/hub/src/App.tsx
@@ -6,36 +6,39 @@ import SignIn from './routes/SignIn'
 import AuthCallback from './routes/AuthCallback'
 import Home from './routes/Home'
 import Workspace from './routes/Workspace'
+import { PushProvider } from './contexts/PushContext'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/auth/callback" element={<AuthCallback />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/workspace"
-            element={<Navigate to="/workspace/vein" replace />}
-          />
-          <Route
-            path="/workspace/:productId"
-            element={
-              <AdminRoute>
-                <Workspace />
-              </AdminRoute>
-            }
-          />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <PushProvider deviceSyncId="9b37a3e7-9b71-4882-a87f-5689913550f7">
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/auth/callback" element={<AuthCallback />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/workspace"
+              element={<Navigate to="/workspace/vein" replace />}
+            />
+            <Route
+              path="/workspace/:productId"
+              element={
+                <AdminRoute>
+                  <Workspace />
+                </AdminRoute>
+              }
+            />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </PushProvider>
   )
 }
